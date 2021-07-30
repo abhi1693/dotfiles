73f4a5c23732022b2401e5c2bb45cf248ed4d607
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2021-07-30 16:36:43 -0700

    Added git global config

--- install.sh
+++ install.sh
@@ -56,3 +56,7 @@ sudo apt install python3 python3-dev python3-pip -y
 
 # Install Pycharm Professional
 sudo snap install pycharm-professional --classic
+
+# Update git global config
+git config --global user.name "Abhimanyu Saharan"
+git config --global user.email "desk.abhimanyu@gmail.com"
